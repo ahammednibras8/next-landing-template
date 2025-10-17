65b5cd58df3fb7824c241fcfd0f62f27ff8451c5
ahammednibras8 <ahammednibras@proton.me>
2025-10-17 18:23:19 +0530

    Removed github pages

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/next-landing-template',
-  assetPrefix: '/next-landing-template',
-  images: {
-    unoptimized: true,
-  },
-  reactStrictMode: true,
-  compress: true,
 };
 
 export default nextConfig;
