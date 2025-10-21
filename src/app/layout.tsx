7aecea729e78b109d30d5477d66450a07af9c314
ahammednibras8 <ahammednibras@proton.me>
2025-10-21 10:05:15 +0530

    Removed Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { metadata } from "@/lib/seo-metadata";
 import { siteConfig } from "@/lib/site-config";
-import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -12,8 +11,6 @@ const inter = Inter({
 });
 
 export { metadata };
-
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({
   children,
@@ -50,28 +47,6 @@ export default function RootLayout({
             }),
           }}
         />
-
-        {/* Google Analytics */}
-        {GA_ID && (
-          <>
-            <Script
-              strategy="afterInteractive"
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-            />
-            <Script
-              id="ga-init"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', { page_path: window.location.pathname });
-                `,
-              }}
-            />
-          </>
-        )}
       </head>
       <body
         className={`${inter.variable} font-sans antialiased bg-neutral-950 text-white`}
